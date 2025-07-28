0445adb392751d283041385df4d5047e4d60a8f0
Caio Yoshii <caio.yoshii@atonanni.com.br>
2025-07-28 16:10:49 -0300

    Implementado a lógica no arquivo JS do projeto

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Criar array para armazenar os nomes
+let amigos = [];
+
+// Função responsável por adicionar amigo a lista
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+// Função responsável por atualizar a lista
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+
+    // 1. Limpa a lista antes de recriar
+    lista.innerHTML = "";
+
+    // 2. Percorre o array e cria um <li> para cada nome
+    for (let i = 0; i < amigos.length; i++) {
+        const item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+// Função responável por sortear
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("A lista está vazia. Adicione amigos antes de sortear.");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const nomeSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>O amigo sorteado foi: <strong>${nomeSorteado}</strong></li>`;
+}
